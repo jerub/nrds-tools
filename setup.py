fcba05d02d3c32e1d3317a32a8789faf86d3f11d
Stephen Thorne <stephen@thorne.id.au>
2012-12-08 17:46:32 +0000

    Remove unneeded kwarg bundle_files=1 to distutils.setup()

--- setup.py
+++ setup.py
@@ -16,6 +16,5 @@ setup(windows=[{'script': 'KosLookupExe.py',
                 'icon_resources': icon_resources}],
     options={'py2exe': {'dll_excludes': ['MSVCP90.dll'],
                         'bundle_files': 1}},
-    bundle_files=1,
     zipfile=None)
 
